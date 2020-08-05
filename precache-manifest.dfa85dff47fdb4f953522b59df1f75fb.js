00c4b71a856596f5ce4686d1d2f9344d00481d2b
Kian Bennett <up940801@myport.ac.uk>
2020-08-05 20:10:45 +0100

    Updates

---
+++ precache-manifest.dfa85dff47fdb4f953522b59df1f75fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1da8dcf45ca24d45de7",
+    "url": "/tcg-collector/static/css/main.b475b72d.chunk.css"
+  },
+  {
+    "revision": "b1da8dcf45ca24d45de7",
+    "url": "/tcg-collector/static/js/main.302ab086.chunk.js"
+  },
+  {
+    "revision": "36c189db67fa4c4309c4",
+    "url": "/tcg-collector/static/js/runtime~main.48777f88.js"
+  },
+  {
+    "revision": "e983fad5900d39fa59c9",
+    "url": "/tcg-collector/static/css/2.facd63ea.chunk.css"
+  },
+  {
+    "revision": "e983fad5900d39fa59c9",
+    "url": "/tcg-collector/static/js/2.7881c332.chunk.js"
+  },
+  {
+    "revision": "ec8c1cc5eb272aa016fce87e0e33d2fd",
+    "url": "/tcg-collector/static/media/logo2.ec8c1cc5.png"
+  },
+  {
+    "revision": "99ad7ff75c8da0169f17cddb23867110",
+    "url": "/tcg-collector/static/media/logo-full.99ad7ff7.png"
+  },
+  {
+    "revision": "842601666c8a3d031431e8702f844007",
+    "url": "/tcg-collector/index.html"
+  }
+];
